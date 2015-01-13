8f3c268ef2eabf27998cd0e620b07b9fee5a1c66
ivy <597055914@qq.com>
2015-01-13 18:25:39 +0800

    update

---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="zh-cn">
+ 	<head>
+ 		<title>HOCG Beta</title>
+ 		<link href="style/style.css" rel="stylesheet">
+ 		<meta http-equiv="Content-Type" content="text/html; charset=utf8" />
+ 		<meta name="Description" content="HOCG">
+		<meta name="keywords" content="SAI,数位板,CG,ACG,动漫,美术,绘画" />
+ 	</head>
+	<body>
+		<div class="left-panel">
+			<div class="panel-heading">
+				<a href="">HOCG</a>
+			</div>
+			<ul class="menu-list">
+				<li class="selected">Latest CG</li>
+				<li class="normal">Memories</li>
+			</ul>
+			<ul class="menu-list">
+				<li class="normal">API Documents</li>
+			</ul>
+			<ul class="menu-list">
+				<li class="normal">Contact</li>
+			</ul>
+		</div>
+	</body>
+</html>
