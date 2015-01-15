111417b6642ca64f86bd5c351b12f4e1c09a0a8a
ivy <597055914@qq.com>
2015-01-15 15:57:38 +0800

    update a lot

---
+++ fuckit/index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="zh-cn">
+ 	<head>
+ 		<title>HoCG administration</title>
+ 		<link href="style/style.css" rel="stylesheet">
+ 		<meta http-equiv="Content-Type" content="text/html; charset=utf8" />
+ 	</head>
+	<body>
+		<div class="login">
+			<div class="login-heading">管理员登录</div>
+			<div class="login-form">
+				<form action="access.php">
+					<input type="text" class="inputarea" placeholder="请输入管理员用户名" autofocus required />
+					<input type="password" class="inputarea" placeholder="请输入管理员密码" required />
+					<input type="submit" class="sub" name="ver" value="登录" />
+				</form>
+			</div>
+		</div>
+	</body>
+</html>
--- latest.php
+++ latest.php
@@ -1,31 +1,32 @@
+<?php
+require('/functions/manager.php');
+error_reporting(E_ALL ^ E_NOTICE);
+
+$pdo=new PDO("mysql:dbname=$dbname;host=$host",$user,$password);
+$cgm=new CGManager($pdo);
+
+$data=$cgm->getLatest();
+
+?>
+
 			<div class="content-panel">
 				<div class="content-heading">
-					<p class="heading-title">2015-1-13</p>
-					<p class="img-desc">This is my CG website</p>
+					<p class="heading-title"><?php echo explode(" ", $data['publicTime'])[0]; ?></p>
+					<p class="img-desc"><?php echo $data['introduction']; ?></p>
 				</div>
 
 				<div class="content-detail">
-					<p class="de-title">沿海的街</p>
-					<P class="de-desc">To create a default inline form, add the pure-form classname to any </p>
+					<p class="de-title"><?php echo $data['title']; ?></p>
+					<P class="de-desc"><?php echo $data['description']; ?></p>
 				</div>
 
 				<div class="content-body-img">
-					<img alt="hhh" src="http://i62.tinypic.com/21vi94.jpg" />
+					<img alt="赞无数据" src="<?php echo $data['imgsrc']; ?>" />
 				</div>
 				
 				<div class="content-tags">
 					<ul class="tags">
-						<li>蛤蛤蛤</li>
-						<li>江泽民</li>
-						<li>江泽民</li>
-						<li>江泽民</li>
-						<li>江泽民</li>
-						<li>江泽民</li>
-						<li>江泽民</li>
-						<li>江泽民</li>
-						<li>江泽民</li>
-						<li>江泽民</li>
-						<li>江泽民</li>
+						<li><?php echo $data['tags']; ?></li>
 					</ul>
 				</div>
 			</div>
