c9170ec581c4d9879bdde9cdccdf28760f2c43e4
ivy <597055914@qq.com>
2015-01-22 21:50:59 +0800

    update

--- fuckit/access.php
+++ fuckit/access.php
@@ -24,4 +24,26 @@ if(!$_SESSION){
 	echo '1002';//已登录
 }
 
+/*  Dim red As Color
+  
+  red = RGB(0, 0, 0, 50)
+  g.ForeColor = red
+  g.FillRect(20, 20,100, 100)
+  g.FillOval(150,120,100,100)
+  g.FillRoundRect(270,200,100,100,4)
+  Dim points() As Integer 
+  points = Array(20, 200, 50,100, 50, 100,100) 
+  g.FillPolygon(points)
+  
+  dim x as Integer
+  dim xStep As Integer
+  
+  x=20
+  xStep=50
+  
+  g.DrawLine(x,x-xStep,x+xStep,x+xStep)
+  g.DrawLine(x-xStep,x+xStep,x,x-xStep)
+  g.DrawLine(x-xStep,x+xStep,x,x+xStep)
+  g.DrawLine(x+xStep,x+xStep,x,x+xStep)*/
+
 ?>
