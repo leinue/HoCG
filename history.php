5816d1711cf845d2715521b2ae58c059d540bf9e
ivy <597055914@qq.com>
2015-01-23 16:28:42 +0800

    update

--- history.php
+++ history.php
@@ -5,6 +5,8 @@
 				</div>
 				<div class="content-detail">
 					<P class="de-desc">HoCG使用PHP+HTML5+CSS3+jQuery+AJAX+MySQL开发</p>
+					<P class="de-desc">2015-1-23 HoCG 1.0.2 发布</p>
+					<P class="de-desc">2015-1-23 修正后台及页面加载时产生的分页BUG</p>
 					<P class="de-desc">2015-1-18 HoCG 1.0.1 修正版 上线</p>
 					<P class="de-desc">2015-1-16 晚 HoCG 1.0 发布</p>
 					<P class="de-desc">2015-1-16 开始整合HoCG的AJAX</p>
